a55d13b6fe918d491de14d032369ecf8a1ffea26
Van Nguyen <thanh_xer@yahoo.com>
2022-01-25 11:05:55 -0600

    complete

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,8 +1,37 @@
 import { getOrders, addCustomOrder } from "./database.js"
+import { getMetals, getSizes, getStyles } from "./database.js"
+
+const metals = getMetals()
+const sizes = getSizes()
+const styles = getStyles()
 
 const buildOrderListItem = (order) => {
+    let totalCost = 0
+    for (const metal of metals) {
+        if (metal.id === order.metalId) {
+            totalCost += metal.price
+        }
+    }
+
+    const temp = sizes.find(size => {
+        if (size.id === order.sizeId) {
+            return totalCost += size.price
+        }
+    })
+
+    for (const style of styles) {
+        if (style.id === order.styleId) {
+            totalCost += style.price
+        }
+    }
+
+    const costString = totalCost.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD"
+    })
+
     return `<li>
-        Order #${order.id} was placed on ${new Date(order.timestamp).toLocaleString()}
+        Order #${order.id} was placed on ${new Date(order.timestamp).toLocaleString()}, costs ${costString}
     </li>`
 }
 
